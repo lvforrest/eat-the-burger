e508479465e6475a3f6f97afdf44dd2818e1ac43
Lacie Forrest <lvforrest@yahoo.com>
2018-05-12 14:27:20 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    port: 8889,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burger_DB"
-});
+var connection
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection= mysql.createConnection({
+        port: 8889,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burger_DB"
+    });
+};
 
 connection.connect(function(err){
     if(err)throw err;
